91f2727f04ef5e90dcce2f9574a33536077ddb71
Ryuji Tsutsui <ryu22e@gmail.com>
2024-08-23 18:59:22 +0900

    jsモジュールを使ったサンプルを追加

---
+++ js-sample/README.md
@@ -0,0 +1,14 @@
+# jsモジュールを使ったアプリケーションのサンプル
+## ローカルでの実行方法
+以下のコマンドを実行する。
+
+```bash
+$ npx wrangler@latest dev
+```
+
+## デプロイ方法
+以下のコマンドを実行する。
+
+```bash
+$ npx wrangler@latest deploy
+```
---
+++ js-sample/src/entry.py
@@ -0,0 +1,17 @@
+from js import Headers, Response, fetch, console
+
+
+API_URL = "https://httpbin.org"
+
+
+async def on_fetch(request, env):
+    # JSON形式でレスポンスを返すためのヘッダーを設定
+    headers = Headers.new({"content-type": "application/json; charset=utf-8"}.items())
+
+    # fetch()関数を使ってAPIサーバーにリクエストを送信
+    res = await fetch(f"{API_URL}/ip")
+
+    # レスポンスの内容をコンソールに出力
+    console.log(res)
+
+    return Response.new(res.body, headers=headers)
